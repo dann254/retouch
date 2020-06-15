591c8b551111d82bc96684e358eee3c3738f7f98
Daniel Kipkemei <dann.professional@gmail.com>
2020-06-16 00:15:35 +0300

    first build
    - working app
    - menu bar icon and button with options
    - app logo

---
+++ README.md
@@ -0,0 +1,41 @@
+# retouch
+Refresh your mackbook touchbaer with ease.
+
+
+Is your macbook touchbar frozen? I got you covered.
+
+- this app gives you a solution for that, easily accessible on your touch bar.
+
+
+## Old ways
+Having to look for the command and pasting it on your terminal is a pain right.
+
+Save your nerves and just click your way to salvation 😃
+
+
+funny: My touch bar froze while I was typing this 🤣. Guess who didn't have trouble refreshing the touchbar😎.
+
+## Mac OS versions
+Designed for MacOs 10.12 Sierra and Above
+
+## Installation
+
+- Visit [the app website]( https://dann254.github.io/retouch/) and click download.
+- Double click the downloaded zip file to extract.
+- Double clik on the extracted `.dmg`
+- Drag `retouch.app` to the `Apllications` folder.
+- wait for the process to finish
+- eject the DMG (by clicking the eject button in the Sidebar)
+
+## Usage
+- Open the app
+- you should see the app Icon appear on the menu bar.
+- Click on the I con and click `Refresh Touchbar` when needed.
+- 🥳
+
+To have the app always open.
+
+- `System Preferences` > `Users & Groups`.
+- Select your User ID.
+- Select `Login Items`.
+- Click the `+` button to select `retouch` app from your `Applications folder`.
--- retouch/AppDelegate.swift
+++ retouch/AppDelegate.swift
@@ -7,18 +7,49 @@
 //
 
 import Cocoa
+import Foundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    var statusBarItem: NSStatusItem!
+    let process = Process()
+    let menu = NSMenu()
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        // Create the status item
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        
+        if let button = self.statusBarItem.button {
+            button.image = NSImage(named: "Icon")
+//            button.action = #selector(restartControlstrip(_:))
+        }
+        menu.addItem(NSMenuItem(title: "Refresh TouchBar", action: #selector(restartControlstrip(_:)), keyEquivalent: "r"))
+        menu.addItem(NSMenuItem(title: "Quit", action: Selector("terminate:"), keyEquivalent: "q"))
+        
+        statusBarItem.menu = menu
     }
+    
+    @objc func restartControlstrip(_ sender: AnyObject?) {
+        @discardableResult
+        func shell(_ command: String) -> String {
+            let task = Process()
+            let pipe = Pipe()
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+            task.standardOutput = pipe
+            task.arguments = ["-c", command]
+            task.launchPath = "/bin/bash"
+            task.launch()
+
+            let data = pipe.fileHandleForReading.readDataToEndOfFile()
+            let output = String(data: data, encoding: .utf8)!
+
+            return output
+        }
+        
+        shell("pkill \"Touch Bar agent\"")
+        shell("killall \"ControlStrip\"")
     }
 
 
