7c4f583d56c4b39b94bd07b74f24003dee0cc403
Daniel Kipkemei <dann.professional@gmail.com>
2020-06-15 19:54:22 +0300

    Initial Commit

---
+++ retouch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  retouch
+//
+//  Created by Daniel Kipkemei on 15/06/2020.
+//  Copyright © 2020 Daniel Kipkemei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ retouch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  retouch
+//
+//  Created by Daniel Kipkemei on 15/06/2020.
+//  Copyright © 2020 Daniel Kipkemei. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
